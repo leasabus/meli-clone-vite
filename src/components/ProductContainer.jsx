54ad0d7c0d0aa7474243c7607a6408a1afa0aabe
Leandro Sabus <leandro.sabus@gmail.com>
2023-02-24 14:31:53 -0300

    container fixed

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -11,13 +11,16 @@ import { SimilarProducts } from './SimilarProducts';
 
 export const ProductContainer = () => {
     return (
-        <div className='h-full max-w-[1200px] flex-col  gap-10 shadow-md bg-white  m-auto px-8 py-4 flex '>
-            <div className='flex flex-col md:flex-row'>
+        <div className='h-full max-w-[1200px] flex-col  gap-10 shadow-md bg-white  m-auto  px-8 py-4 flex '>
+            <div className='flex flex-col   md:flex-row'>
                 <ProductComponent images={[image1, image2, image3, image4]} />
-                <ProductInformation />
+                <div className='flex flex-col items-center'>
+                    <ProductInformation />
+                </div>
             </div>
 
             <SimilarProducts />
+
 
 
         </div>
